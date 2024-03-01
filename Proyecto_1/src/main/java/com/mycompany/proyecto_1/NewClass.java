d5b5c5e3fa50ea842e5f29a3be62c7d8ec8a203d
JesusJK <camilojesus19@gmail.com>
2024-03-01 12:07:31 -0500

    creacion de paquetes, gretters y setters

--- Proyecto_1/src/main/java/com/mycompany/proyecto_1/NewClass.java
+++ Proyecto_1/src/main/java/com/mycompany/proyecto_1/NewClass.java
@@ -1,7 +1,9 @@
 package com.mycompany.proyecto_1;
+import character.Character; 
 public class NewClass {
     
     public static void main(String[] args){
+        
         Character homero = new Character("Homero","inspector de seguridad de una planta nuclear",38,"pequeño demonio");
         homero.greeting();
         System.out.println(homero.age);
@@ -9,24 +11,4 @@ public class NewClass {
         Character bart = new Character("bart","vago",10, "ay caramba");
         bart.greeting();
     }
-    static class Character{
-        String name;
-        String occupation;
-        int age;
-        String phrase;
-
-        public Character(String name, String occupation, int age, String phrase) {
-            this.name = name;
-            this.occupation = occupation;
-            this.age = age;
-            this.phrase = phrase;
-        }
-         
-        
-        void greeting(){
-            System.out.println(this.phrase);
-        }
-        
-    }
-    
 }
