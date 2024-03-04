5b4b849bb4959f33c561ee06ea74f15f2d55ddb3
JesusJK <camilojesus19@gmail.com>
2024-03-04 10:28:32 -0500

    parche de coreccion

--- Proyecto_1/src/main/java/com/mycompany/proyecto_1/NewClass.java
+++ Proyecto_1/src/main/java/com/mycompany/proyecto_1/NewClass.java
@@ -6,7 +6,7 @@ public class NewClass {
         
         Character homero = new Character("Homero","inspector de seguridad de una planta nuclear",38,"pequeño demonio");
         homero.greeting();
-        System.out.println(homero.age);
+        
         
         Character bart = new Character("bart","vago",10, "ay caramba");
         bart.greeting();
